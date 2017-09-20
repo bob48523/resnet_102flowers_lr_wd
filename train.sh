b2810f05c79cee45b3c842a7aaa696d1ba174e0d
bob48523 <be741644@mail.ustc.edu.cn>
2017-09-20 16:18:09 +0800

    ...

---
+++ train.sh
@@ -0,0 +1,13 @@
+#python train.py --save='work/lr/lr0.3' --lr=0.3 &&
+#python train.py --save='work/lr/lr0.1' --lr=0.1 &&
+#python train.py --save='work/lr/lr0.03' --lr=0.1 &&
+#python train.py --save='work/lr/lr0.01' --lr=0.1 &&
+#python train.py --save='work/lr/lr0.003' --lr=0.1 &&
+#python train.py --save='work/lr/lr0.001' --lr=0.1 &&
+#python train.py --save='work/wd/wd0.003' --wd=0.003 &&
+#python train.py --save='work/wd/wd0.001' --wd=0.001 &&
+#python train.py --save='work/wd/wd0.0007' --wd=0.0007 &&
+#python train.py --save='work/wd/wd0.0003' --wd=0.0003 &&
+#python train.py --save='work/wd/wd0.004' --wd=0.004 && 
+python train.py --save='work/optimal' --wd=0.003 --lr=0.003
+
